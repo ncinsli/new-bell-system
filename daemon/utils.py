cd1ee3e36b69d6464bc4c451007a76b3b8ba9fd3
ncinsli <56553344+ncinsli@users.noreply.github.com>
2023-04-07 15:32:03 +0500

    Оптимизация запуска

--- daemon/utils.py
+++ daemon/utils.py
@@ -4,7 +4,8 @@ def nearest_forward_ring_index(table):
     nowtime = [datetime.now().hour, datetime.now().minute]
     nearest = -1
     for i in range(len(table)):
-        if int(table[i].split(":")[0]) > nowtime[0] or (int(table[i].split(":")[0]) == nowtime[0] and int(table[i].split(":")[1]) > nowtime[1]):
+        tsplit = table[i].split(":")
+        if int(tsplit[0]) > nowtime[0] or (int(tsplit[0]) == nowtime[0] and int(tsplit[1]) > nowtime[1]):
             nearest = i
             break
 
