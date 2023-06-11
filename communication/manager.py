1872a477ff538fbaa3e7ded895adfd42aa7557f2
xfx1337 <tttfff1338@gmail.com>
2023-06-11 14:13:15 +0500

    processes stability update. on startup sync added

--- communication/manager.py
+++ communication/manager.py
@@ -19,6 +19,7 @@ class NetManager(threading.Thread):
         self.wait = False
         self.readids = []
         self.daemon = True # threading
+        self.sio = None
 
         self.processes =  {}
     
@@ -42,7 +43,7 @@ class NetManager(threading.Thread):
         
         if self.work:
             sio = socketio.Client()
-
+            self.sio = sio
             @sio.event(namespace="/refreshing")
             def interrupt(data):
                 if self.device_id in data["ids"]:
@@ -56,11 +57,20 @@ class NetManager(threading.Thread):
             @sio.event(namespace="/refreshing")
             def connect():
                 print("[NETMANAGER] connecting established")
+                print("[NETMANAGER] syncing processes")
+                self.sync_processes()
                 self.work = True
+                refreshing_task_thread = sio.start_background_task(refreshing_task)
 
             @sio.event(namespace="/refreshing")
             def disconnect():
                 print("[NETMANAGER] disonnected")
+                self.work = False
+                threading.Thread(target=self.retry_login, args=(self.device_id, "Zvonki2023", ), daemon=True).start()
+                while not self.work:
+                    time.sleep(0.1)
+                    pass
+                time.sleep(3)
                 self.work = False
                 reconnect_task_thread = sio.start_background_task(reconnection_task)
 
@@ -70,6 +80,8 @@ class NetManager(threading.Thread):
                 sio.disconnect()
 
             def refreshing_task():
+                if self.work:
+                    print("[NETMANAGER] refreshing task started")
                 while self.work:
                     stats = self.get_system_stats()
                     stats["token"] = self.token
@@ -86,7 +98,6 @@ class NetManager(threading.Thread):
                         print("[NETMANAGER] reconnecting")
                         sio.connect(self.host, headers={"Authorization": "Bearer " + self.token}, namespaces=["/refreshing"], wait_timeout = 10)
                         self.work = True
-                        refreshing_task_thread = sio.start_background_task(refreshing_task)
                         sio.wait()
                         break
                     except:
@@ -94,13 +105,13 @@ class NetManager(threading.Thread):
                     time.sleep(5)
             
             while self.work:
-                    try:
-                        sio.connect(self.host, headers={"Authorization": "Bearer " + self.token}, namespaces=["/refreshing"], wait_timeout = 10)
-                        refreshing_task_thread = sio.start_background_task(refreshing_task)
-                        sio.wait()
-                        break
-                    except:
-                        pass            
+                try:
+                    sio.connect(self.host, headers={"Authorization": "Bearer " + self.token}, namespaces=["/refreshing"], wait_timeout = 10)
+                    refreshing_task_thread = sio.start_background_task(refreshing_task)
+                    sio.wait()
+                    break
+                except:
+                    pass            
             
 
     def register(self, id):
@@ -154,13 +165,13 @@ class NetManager(threading.Thread):
     
     def try_request(self, data, sio):
         print('[NETMANAGER] parsing request')
-        print(data)
 
         if data["ids"] != "all":
             if self.device_id not in data["ids"]:
                 return
 
         if data["type"] == "execute":
+            print(f"[NETMANAGER] starting process. execution_id: {data['content']['execution_id']}")
             if data["content"]["failsafe_mode"]:
                 threading.Thread(target=self.killer, args=(data["content"]["failsafe_timeout"], data["content"]["execution_id"])).start()
             self.processes[data["content"]["execution_id"]] = subprocess.Popen(data["content"]["cmd"], stdout=subprocess.PIPE, shell=True)
@@ -179,12 +190,15 @@ class NetManager(threading.Thread):
             except: pass
         elif data["type"] == "interrupt":
             if data["execution_id"] == "all":
-                for p in self.processes.keys():
-                    self.kill_process(p)
+                keys = list(self.processes.keys()).copy()
+                for p in keys:
+                    try: self.kill_process(p)
+                    except: pass
                 self.processes = {}
+
             else:
                 if data["execution_id"] not in self.processes.keys():
-                    sio.emit("device_response", {"type": "device_response", "response": "", "execution_id": data["execution_id"], "errors": "Process not started", "token": self.token}, namespace="/refreshing")
+                    #sio.emit("device_response", {"type": "device_response", "response": "", "execution_id": data["execution_id"], "errors": "Process not started", "token": self.token}, namespace="/refreshing")
                     return
                 
                 self.kill_process(data["execution_id"])
@@ -196,7 +210,7 @@ class NetManager(threading.Thread):
         return r.status_code, r.text
 
     def killer(self, timeout, execution_id):
-        time.sleep(timeout)
+        time.sleep(float(timeout))
         self.kill_process(execution_id)
 
     def kill_process(self, execution_id):
@@ -207,7 +221,33 @@ class NetManager(threading.Thread):
                 kill_child_processes(self.processes[execution_id].pid)
         except:
             try: print(f"[NETMANAGER] couldn't kill process. execution_id: {execution_id}. pid: {self.processes[execution_id].pid}")
-            except: print("[NETMANAGER] something went wrong while killing process")
+            except: print("[NETMANAGER] couldn't kill process. process was already killed")
+
+    def sync_processes(self):
+        r = requests.post(self.host + "/api/devices/sync_processes", headers={"Authorization": "Bearer " + self.token})    
+        processes = r.json()["processes"]
+        for p in processes:
+            threading.Thread(target=self._process_thread, args=(p, ), daemon=True).start()
+    
+    def _process_thread(self, process):
+        print(f"[NETMANAGER] starting process. execution_id: {process['execution_id']}")
+        if process["failsafe_mode"]:
+            threading.Thread(target=self.killer, args=(process["failsafe_timeout"], process["execution_id"])).start()
+        self.processes[process["execution_id"]] = subprocess.Popen(process["cmd"], stdout=subprocess.PIPE, shell=True)
+        out, err = self.processes[process["execution_id"]].communicate()
+        if out != None: 
+            out = out.decode("utf-8")
+        else:
+            out = ""
+        if err != None:
+            err = err.decode("utf-8")
+        else:
+            err = ""
+
+        self.sio.emit("device_response", {"type": "device_response", "response": out, "execution_id": process["execution_id"], "errors": err, "token": self.token}, namespace="/refreshing")
+        try: del self.processes[process["execution_id"]]
+        except: pass
+
 
 def kill_child_processes(parent_pid, sig=signal.SIGTERM):
     try:
